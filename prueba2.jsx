15f57ade6dd180a5497418cb37c1a4aacf4e165f
Alejandro Lluch <alejandrolluchsanchez@outlook.es>
2023-10-30 17:00:30 +0100

    añadido nose que

--- prueba2.jsx
+++ prueba2.jsx
@@ -6,4 +6,14 @@ const prueba2 = () => {
   )
 }
 
+import React from 'react'
+
+const prueba2 = () => {
+  return (
+    <div>prueba2</div>
+  )
+}
+
+export default prueba2
+
 export default prueba2
